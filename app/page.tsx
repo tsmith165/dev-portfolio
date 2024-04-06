6b9a056235d67085cc64215bb2175d73730e3301
tsmith165 <torreysmith165@gmail.com>
2024-04-06 15:37:50 -0700

    updating volunteer animations and updating main page navbar layout

--- app/page.tsx
+++ app/page.tsx
@@ -38,7 +38,7 @@ export default function Home() {
     return (
         <PageLayout page="/">
             <div className="bg-gradient-to-b from-primary to-primary_dark to-20%">
-                <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-12">
+                <div className="max-w-full px-4 sm:px-6 lg:px-8 py-12">
                     <Hero />
                     <Technologies />
                     <Experience />
--- components/framer_motion/Volunteering.tsx
+++ components/framer_motion/Volunteering.tsx
@@ -27,21 +27,17 @@ export default function Volunteering() {
                 {volunteering.map((vol, index) => (
                     <motion.div
                         key={vol.organization}
-                        initial={{ opacity: 0, y: 50 }}
-                        whileInView={{ opacity: 1, y: 0 }}
+                        initial={{ opacity: 0, x: index % 2 === 0 ? -50 : 50 }}
+                        whileInView={{ opacity: 1, x: 0 }}
                         viewport={{ once: false }}
                         transition={{ duration: 0.5, delay: index * 0.2 }}
                         className="flex flex-col md:flex-row items-start md:items-center">
                         <div className="w-full md:w-1/3 md:text-right md:pr-8">
-                            <h3 className="text-xl font-bold ">
-                                <span className="bg-gradient-to-r from-secondary_dark to-secondary_light text-transparent bg-clip-text">
-                                    {vol.organization}
-                                </span>
-                            </h3>
-                            <p className="text-lg text-secondary_light md:mt-2">{vol.duration}</p>
+                            <h3 className="text-xl font-bold text-secondary">{vol.organization}</h3>
+                            <p className="text-lg text-secondary md:mt-2">{vol.duration}</p>
                         </div>
                         <div className="w-full md:w-2/3 mt-2 md:mt-0">
-                            <p className="text-lg font-bold text-secondary">{vol.position}</p>
+                            <p className="text-lg font-bold text-secondary_dark">{vol.position}</p>
                             <p className="mt-2 text-secondary">{vol.description}</p>
                         </div>
                     </motion.div>
