71118c37856022189a2c6b249128e2fd413e6802
tsmith165 <torreysmith165@gmail.com>
2024-04-06 15:42:46 -0700

    updating volunteer gradients

--- components/framer_motion/Volunteering.tsx
+++ components/framer_motion/Volunteering.tsx
@@ -33,11 +33,15 @@ export default function Volunteering() {
                         transition={{ duration: 0.5, delay: index * 0.2 }}
                         className="flex flex-col md:flex-row items-start md:items-center">
                         <div className="w-full md:w-1/3 md:text-right md:pr-8">
-                            <h3 className="text-xl font-bold text-secondary">{vol.organization}</h3>
+                            <h3 className="text-xl font-bold ">
+                                <span className="bg-gradient-to-r from-secondary_dark to-secondary_light text-transparent bg-clip-text">
+                                    {vol.organization}
+                                </span>
+                            </h3>
                             <p className="text-lg text-secondary md:mt-2">{vol.duration}</p>
                         </div>
                         <div className="w-full md:w-2/3 mt-2 md:mt-0">
-                            <p className="text-lg font-bold text-secondary_dark">{vol.position}</p>
+                            <p className="text-lg font-bold text-secondary">{vol.position}</p>
                             <p className="mt-2 text-secondary">{vol.description}</p>
                         </div>
                     </motion.div>
